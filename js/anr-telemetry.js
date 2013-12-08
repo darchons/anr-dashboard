f8dc512acaeee6b8ed7be79461591b631a5934db
Jim Chen <nchen@mozilla.com>
2013-12-08 18:50:26 -0500

    Add initial version

---
+++ js/anr-telemetry.js
@@ -0,0 +1,284 @@
+(function(exports) {
+
+"use strict";
+
+function ANRTelemetry() {
+}
+
+ANRTelemetry.prototype = {
+
+    _get: function(file, cb) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", this._root + file, true);
+        xhr.onload = function() {
+            cb(JSON.parse(xhr.responseText));
+        };
+        xhr.onerror = function(e) {
+            throw new Error("ANRTelemetry: failed to retrieve file.");
+        };
+        xhr.send(null);
+    },
+
+    init: function(rootUri, cb) {
+        if (rootUri.lastIndexOf("/") != rootUri.length - 1) {
+            rootUri = rootUri + "/";
+        }
+        this._root = rootUri
+        this._dimensions = {};
+
+        var self = this;
+        this._get("index.json", function(index) {
+            self.index = index;
+            cb(self);
+        });
+    },
+
+    getDimensions: function() {
+        return Object.keys(this.index.dimensions);
+    },
+
+    getDimension: function(dim, cb) {
+        if (!this.index.dimensions[dim]) {
+            throw new Error("ANRTelemetry: invalid dimension.");
+        }
+        var self = this;
+        function _getCachedDimension() {
+            return cb(new Dimension(self, dim, self._dimensions[dim]));
+        }
+        if (this._dimensions[dim]) {
+            return _getCachedDimension();
+        }
+        this._get(this.index.dimensions[dim], function(content) {
+            self._dimensions[dim] = content;
+            _getCachedDimension();
+        });
+    },
+
+    _getThreads: function(threads, cb) {
+        return cb(threads.map(function(thread) {
+            return new Thread(thread);
+        }));
+    },
+
+    _getMainThread: function(key, cb) {
+        if (this._mainThread) {
+            return this._getThreads([this._mainThread[key]], cb);
+        }
+        var self = this;
+        this._get(this.index.main_thread, function(content) {
+            self._mainThread = content;
+            this._getThreads([this._mainThread[key]], cb);
+        });
+    },
+
+    _getBackgroundThreads: function(key, cb) {
+        if (this._backgroundThreads) {
+            return this._getThreads(this._backgroundThreads[key], cb);
+        }
+        var self = this;
+        this._get(this.index.background_threads, function(content) {
+            self._backgroundThreads = content;
+            this._getThreads(this._backgroundThreads[key], cb);
+        });
+    },
+};
+
+function Dimension(anr, dim, content) {
+    this._anr = anr;
+    this._dim = dim;
+    this._content = content;
+    this._value_agg = {};
+}
+
+Dimension.prototype = {
+
+    getName: function() {
+        return this._dim;
+    },
+
+    getValues: function() {
+        var values = {};
+        for (var key in this._content) {
+            var anr = this._content[key];
+            for (var value in anr) {
+                values[value] = null;
+            }
+        }
+        return Object.keys(values);
+    },
+
+    getANRCount: function() {
+        var agg = this.getAggregate();
+        var max = 0;
+        for (var info in agg) {
+            var histogram = agg[info];
+            var count = 0;
+            for (var value in histogram) {
+                count += histogram[value];
+            }
+            max = Math.max(max, count);
+        }
+        return max;
+    },
+
+    getUniqueANRCount: function() {
+        return Object.keys(this._content).length;
+    },
+
+    getANRs: function() {
+        var self = this;
+        return Object.keys(this._content).map(function(key) {
+            return new ANR(self._anr, key, self._content[key]);
+        });
+    },
+
+    _aggregate: function(agg, histograms) {
+        for (var info in histograms) {
+            var agg_histogram = agg[info] || {};
+            var histogram = histograms[info];
+            for (var value in histogram) {
+                agg_histogram[value] =
+                    (agg_histogram[value] || 0) + histogram[value];
+            }
+            agg[info] = agg_histogram;
+        }
+    },
+
+    getAggregateByValue: function(value) {
+        if (!this._value_agg[value]) {
+            var self = this;
+            this._value_agg[value] = Object.keys(this._content)
+                                           .reduce(function(prev, key) {
+                self._aggregate(prev, self._content[key][value]);
+                return prev;
+            }, {});
+        }
+        return this._value_agg[value];
+    },
+
+    getAggregate: function() {
+        if (!this._agg) {
+            var self = this;
+            this._agg = Object.keys(this._content).reduce(function(prev, key) {
+                var anr = self._content[key];
+                for (var value in anr) {
+                    self._aggregate(prev, anr[value]);
+                }
+                return prev;
+            }, {});
+        }
+        return this._agg;
+    },
+
+    filter: function() {
+    },
+};
+
+function ANR(anr, key, value_histograms) {
+    this._anr = anr;
+    this._key = key;
+    this._value_histograms = value_histograms;
+    this._value_count = {};
+}
+
+ANR.prototype = {
+
+    getMainThread: function(cb) {
+        return this._anr._getMainThread(this._key, cb);
+    },
+
+    getBackgroundThreads: function(cb) {
+        return this._anr._getBackgroundThreads(this._key, cb);
+    },
+
+    _countHistograms: function(histograms) {
+        var max = 0;
+        for (var info in histograms) {
+            var count = 0;
+            var histogram = histograms[info];
+            for (var value in histogram) {
+                count += histogram[value];
+            }
+            max = Math.max(max, count);
+        }
+        return max;
+    },
+
+    getCountByValue: function(value) {
+        if (!this._value_count[value]) {
+            this._value_count[value] =
+                this._countHistograms(this._value_histograms[value]);
+        }
+        return this._value_count[value];
+    },
+
+    getCount: function() {
+        if (!this._count) {
+            var count = 0;
+            for (var value in this._value_histograms) {
+                count += this._countHistograms(this._value_histograms[value]);
+            }
+            this._count = count;
+        }
+        return this._count;
+    },
+};
+
+function Thread(thread) {
+    this._thread = thread;
+}
+
+Thread.prototype = {
+
+    getName: function() {
+        return this._thread.name;
+    },
+
+    getStack: function() {
+        if (!this._stack) {
+            this._stack = this._thread.stack.map(function(frame) {
+                return new StackFrame(frame);
+            });
+        }
+        return this._stack;
+    },
+};
+
+function StackFrame(frame) {
+    this._components = frame.split(':');
+}
+
+StackFrame.prototype = {
+
+    isNative: function() {
+        return this._components[0] === "c";
+    },
+
+    isJava: function() {
+        return this._components[0] === "j";
+    },
+
+    getFunction: function() {
+        if (this.isJava()) {
+            return this._components[1];
+        } else if (this.isNative()) {
+            return this._components[2];
+        }
+    },
+
+    getLib: function() {
+        if (this.isNative()) {
+            return this._components[1];
+        }
+    },
+
+    getLine: function() {
+        if (this.isJava()) {
+            return this._components[2];
+        }
+    },
+};
+
+exports.ANRTelemetry = ANRTelemetry;
+
+})(this);
---
+++ js/dashboard.js
@@ -0,0 +1,246 @@
+$(function() {
+
+"use strict";
+
+var anrTelemetry = null;
+var serverUri = "http://people.mozilla.org/~nchen/anrs/anr-{from}-{to}";
+var defaultDimension = "submission_date";
+var topANRs = 10;
+var anrColors = (function() {
+    var colors = [];
+    for (var i = 0; i <= topANRs; i++) {
+        colors.push(Color({
+            h: 222 - 222 * i / topANRs,
+            s: 55,
+            l: 55,
+        }).hexString());
+    }
+    return colors;
+})();
+
+$(".plot").each(function(i, plot) {
+    $(plot).height($(plot).parent().height() - $(plot).position().top);
+});
+
+$("#navbar-filter").popover({
+    html: true,
+    content: function() {
+        return $("#popover-filter").html();
+    }
+});
+
+function replotANR(elem, dim) {
+    var values = dim.getValues();
+    values.sort();
+
+    var anrs = dim.getANRs();
+    anrs.sort(function(anr1, anr2) {
+        return anr1.getCount() - anr2.getCount();
+    });
+    var otherANRs = anrs.slice(0, -topANRs);
+
+    var data = [{
+        label: "other",
+        data: values.map(function(value, index) {
+            return [index, otherANRs.reduce(function(prev, anr) {
+                return prev + anr.getCountByValue(value);
+            }, 0)];
+        })
+    }];
+    anrs.slice(-topANRs).forEach(function(anr) {
+        data.push({
+            data: values.map(function(value, index) {
+                return [index, anr.getCountByValue(value)];
+            })
+        });
+    });
+
+    $.plot(elem, data, {
+        series: {
+            stack: true,
+            bars: {
+                show: true,
+                align: "center",
+                barWidth: 0.9,
+            },
+        },
+        grid: {
+            show: true,
+            clickable: true,
+            hoverable: true,
+        },
+        xaxis: {
+            ticks: values.map(function(value, index) {
+                return [index, value];
+            }),
+        },
+        colors: anrColors,
+    });
+}
+
+function replotInfo(elem, dim, value) {
+    var agg = value ? dim.getAggregateByValue(value)
+                    : dim.getAggregate();
+
+    var seriescount = 0;
+    var infos = Object.keys(agg);
+    var data = infos.map(function(info, index) {
+        var histogram = agg[info];
+        var valuesarray = Object.keys(histogram);
+        seriescount = Math.max(seriescount, valuesarray.length);
+        valuesarray = valuesarray.map(function(value) {
+            return [value, histogram[value]];
+        });
+        valuesarray.sort(function(val1, val2) {
+            return val2[1] - val1[1];
+        });
+        var total = valuesarray.reduce(function(prev, value) {
+            return prev + value[1];
+        }, 0) / 100;
+        return valuesarray.map(function(value) {
+            return {info: value[0], data: [value[1] / total, index]};
+        });
+    });
+
+    var plotdata = [];
+    data.forEach(function(info, infoindex) {
+        var prevmapto = -1;
+        info.forEach(function(series, index) {
+            var mapto = Math.max(prevmapto + 1, Math.round(100 - series.data[0]));
+            for (var i = prevmapto + 1; i < mapto; i++) {
+                plotdata[i] = (plotdata[i] || {
+                    data: [],
+                    anr_info: [],
+                });
+                plotdata[i].data.push([0, series.data[1]]);
+                plotdata[i].anr_info.push(null);
+            }
+            plotdata[mapto] = (plotdata[mapto] || {
+                data: [],
+                anr_info: [],
+            });
+            plotdata[mapto].data.push(series.data);
+            plotdata[mapto].anr_info.push(series.info);
+            prevmapto = mapto;
+        });
+    });
+
+    var colors = [];
+    for (var i = 0; i <= 100; i++) {
+        var scale = Math.pow(i / 100, 4);
+        colors.push(Color({
+            h: 177 * scale + 22,
+            s: 44,
+            l: 55,
+        }).hexString());
+    }
+    for (var i = 0; i < seriescount; i++) {
+        colors.push(Color({h: 200, s: 44, l: 55}).hexString());
+    }
+
+    function _tooltip(label, xval, yval, item) {
+        return item.series.anr_info[item.dataIndex] + ": " +
+               Math.round(item.series.data[item.dataIndex][0]) + "%";
+    }
+    function _tooltipHover(item, tooltip) {
+        var baroffset = plotobj.pointOffset({
+            x: (item.datapoint[0] + item.datapoint[2]) / 2,
+            y: item.datapoint[1] - 0.5,
+        });
+        var plotoffset = elem.offset();
+        tooltip.html(
+            "<div class='tooltip-inner'>" + tooltip.html() + "</div>" +
+            "<div class='tooltip-arrow'></div>")
+        .offset({
+            left: plotoffset.left + baroffset.left - tooltip.width() / 2,
+            top: plotoffset.top + baroffset.top,
+        });
+    }
+    var plotobj = $.plot(elem, plotdata, {
+        series: {
+            stack: true,
+            bars: {
+                show: true,
+                align: "center",
+                barWidth: 0.6,
+                horizontal: true,
+            },
+        },
+        grid: {
+            show: true,
+            color: "transparent",
+            hoverable: true,
+        },
+        yaxis: {
+            show: true,
+            ticks: Object.keys(agg).map(function(info, index) {
+                return [index, info];
+            }),
+        },
+        xaxis: {
+            show: false,
+        },
+        colors: colors,
+        tooltip: true,
+        tooltipOpts: {
+            content: _tooltip,
+            onHover: _tooltipHover,
+        },
+    });
+}
+
+$("#navbar-groupby").change(function() {
+    var val = $("#navbar-groupby").val();
+    if (!val) {
+        $.plot($("#anr-plot"), [[0, 0]], {grid: {show: true}});
+        $.plot($("#info-plot"), [[0, 0]], {grid: {show: false}});
+        $("#info-dim-value").empty().off("change");
+        return $("#navbar-count").text(0);
+    }
+    anrTelemetry.getDimension(val, function(dim) {
+        $("#navbar-count").text(dim.getANRCount());
+        replotANR($("#anr-plot"), dim);
+
+        var infodim = $("#info-dim-value").empty().off("change");
+        var values = dim.getValues();
+        values.sort();
+        values.unshift("(all groups)");
+        values.forEach(function(value) {
+            infodim.append($("<option/>").text(value))
+        });
+        infodim[0].selectedIndex = 0;
+        infodim.change(function() {
+            replotInfo($("#info-plot"), dim,
+                infodim[0].selectedIndex == 0 ? null : infodim.val());
+        }).trigger("change");
+    });
+});
+
+$("#navbar-from").change(function() {
+    var toDate = Date.today().last().saturday();
+    if (Date.today().isBefore(
+            toDate.clone().next().day()
+                .add(-toDate.getTimezoneOffset()).minutes()
+                .add(8 /* PST */).hours())) {
+        toDate.last().saturday();
+    }
+    toDate = toDate.add(-$("#navbar-from")[0].selectedIndex).weeks();
+
+    var fromDate = toDate.clone().last().sunday();
+    var uri = serverUri.replace("{from}", fromDate.toString("yyyyMMdd"))
+                       .replace("{to}", toDate.toString("yyyyMMdd"));
+
+    var groupby = $("#navbar-groupby").empty().trigger("change");
+
+    anrTelemetry = new ANRTelemetry();
+    anrTelemetry.init(uri, function() {
+        var dims = anrTelemetry.getDimensions();
+        dims.sort();
+        dims.forEach(function(dim) {
+            groupby.append($("<option/>").text(dim));
+        });
+        groupby.val(defaultDimension).trigger("change");
+    });
+}).trigger("change");
+
+});
